82e0d3d578645661fb411738dfb123adc5df6763
Daniel Lee <lee.daniel84@gmail.com>
2016-05-03 22:39:10 -0600

    Initil Commit

---
+++ Nav Mobile Assignment/Nav Mobile Assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nav Mobile Assignment
+//
+//  Created by Daniel Lee on 5/3/16.
+//  Copyright © 2016 Daniel Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
